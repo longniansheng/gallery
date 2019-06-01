bfcaa8938fba7c83373d72697feeacfbe3a041d2
wangtao <18516838863@163.com>
2019-06-01 22:34:02 +0800

    chore: 示例页面

---
+++ src/demo/precache-manifest.53589f3204d8b379f8ba868f66f251d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4123411ef85aff989ded569ddee461",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec3e738d306a67069e14",
+    "url": "/static/css/main.42abd0d3.chunk.css"
+  },
+  {
+    "revision": "df95ffd4a69492b5a5f7",
+    "url": "/static/js/2.0bfff742.chunk.js"
+  },
+  {
+    "revision": "ec3e738d306a67069e14",
+    "url": "/static/js/main.a2687a69.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "51f07281d04c9f78016ff6228670ba24",
+    "url": "/static/media/iconfont.51f07281.woff"
+  }
+]);
